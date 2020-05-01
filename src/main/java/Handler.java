32937e727e5f4028357cb623b62aaa71ad463dd2
Lucas Antunes <lucasba8@gmail.com>
2020-05-01 17:08:08 -0300

    Tasks are now filtered by service name, add some documentation

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -9,8 +9,9 @@ public class Handler implements RequestHandler<Map<String,String>,String>
     {
         String clusterARN = System.getenv("CLUSTER_ARN");
         String hostedZoneId = System.getenv("HOSTED_ZONE_ID");
+        String serviceName = System.getenv("SERVICE_NAME");
 
-        HostnameMaintainer.fixInvalidRecordsAndOrphanTasks(clusterARN, hostedZoneId);
+        HostnameMaintainer.fixInvalidRecordsAndOrphanTasks(clusterARN, serviceName, hostedZoneId);
 
         return "200 OK";
     }
--- src/main/java/HostnameMaintainer.java
+++ src/main/java/HostnameMaintainer.java
@@ -9,17 +9,110 @@ import com.amazonaws.services.route53.model.*;
 
 public class HostnameMaintainer
 {
-    public static List<String> getTaskIPs(String clusterARN)
+    /**
+     * Assigns orphan task IPs with invalid records.
+     * Tasks that don't have their private IP associated with a record set
+     * on the provided hosted zone are called orphan tasks.
+     * A type record sets that have IPs which don't belong to tasks in the specified
+     * cluster are called invalid records.
+     * @param clusterARN ARN of the cluster
+     * @param hostedZoneId id of the hosted zone to manage
+     * @param serviceName id of the service that owns the tasks
+     */
+    public static void fixInvalidRecordsAndOrphanTasks(String clusterARN, String serviceName, String hostedZoneId)
+    {
+        List<String> orphanTaskIPs = getTaskIPs(clusterARN, serviceName);
+        Map<String, String> recordsMap = getRecordNameIpMap(hostedZoneId);
+
+        //Records whose IPs are not present in taskIps
+        List<String> invalidRecords = new ArrayList<>();
+
+        //Changes to be applied to Route53
+        List<Change> changes = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : recordsMap.entrySet())
+        {
+            String recordName = entry.getKey();
+            String recordIP = entry.getValue();
+
+            //Remove recordIP from orphanTaskIPs and
+            //if it failed to remove (the recordIP was not present
+            //in orphanTaskIPs) then we add the recordName
+            //to invalidRecords
+            if(!orphanTaskIPs.remove(recordIP))
+            {
+                invalidRecords.add(recordName);
+            }
+        }
+
+        //Assign orphan IPs to invalid records
+        for (String ip : orphanTaskIPs)
+        {
+            if(invalidRecords.size() > 0)
+            {
+                //Make a change to update an invalid record with
+                //an orphan IP
+
+                Change change = new Change();
+                ResourceRecordSet resourceRecordSet = new ResourceRecordSet();
+
+                resourceRecordSet.setName(invalidRecords.get(0));
+                resourceRecordSet.setType("A");
+                resourceRecordSet.setTTL((long) 300);
+
+                ResourceRecord resourceRecord = new ResourceRecord();
+                resourceRecord.setValue(ip);
+                resourceRecordSet.setResourceRecords(Collections.singletonList(resourceRecord));
+
+                change.setAction("UPSERT");
+                change.setResourceRecordSet(resourceRecordSet);
+
+                changes.add(change);
+
+                //Remove the first element of invalidRecords since we just made a change that
+                //assigns an IP to it and it's therefore not an invalid record anymore.
+                invalidRecords.remove(0);
+            }
+            else
+            {
+                //Ran out of records to assign IPs
+                //Issue an error
+                break;
+            }
+        }
+
+        //Only make a route 53 change request if there are any changes to be made
+        if(changes.size() > 0)
+        {
+            AmazonRoute53 r53 = AmazonRoute53ClientBuilder.defaultClient();
+
+            ChangeBatch changeBatch = new ChangeBatch();
+            changeBatch.setChanges(changes);
+
+            ChangeResourceRecordSetsRequest request = new ChangeResourceRecordSetsRequest();
+            request.setChangeBatch(changeBatch);
+            request.setHostedZoneId(hostedZoneId);
+
+            r53.changeResourceRecordSets(request);
+        }
+    }
+
+    /**
+     * Returns a List containing private IPs from all Fargate tasks that belong to a service.
+     * @param clusterARN the ARN of the cluster of the tasks
+     * @return an array of private IPs
+     */
+    public static List<String> getTaskIPs(String clusterARN, String serviceName)
     {
         AmazonECS ecs = AmazonECSClientBuilder.defaultClient();
 
         ListTasksRequest listRequest = new ListTasksRequest();
         listRequest.setCluster(clusterARN);
         listRequest.setDesiredStatus(DesiredStatus.RUNNING);
+        listRequest.setLaunchType("FARGATE");
+        listRequest.setServiceName(serviceName);
 
         List<String> taskARNs = ecs.listTasks(listRequest).getTaskArns();
-
-        System.out.println(taskARNs);
 
         DescribeTasksRequest describeTasksRequest = new DescribeTasksRequest();
         describeTasksRequest.setCluster(clusterARN);
@@ -56,6 +149,14 @@ public class HostnameMaintainer
         return privateIPs;
     }
 
+    /**
+     * Returns a map of recordName => IP from Route 53
+     * Only searches for 'A' type records.
+     * If the record contains more than one IP assigned to it the entire record will be ignored
+     * (i.e. it won't be present in the returned map).
+     * @param hostedZoneId the ID of the hosted zone from which to get the data
+     * @return a recordName => IP map
+     */
     public static Map<String, String> getRecordNameIpMap(String hostedZoneId)
     {
         Map<String, String> retVal = new HashMap<>();
@@ -84,78 +185,6 @@ public class HostnameMaintainer
         }
 
         return retVal;
-    }
-
-    public static void fixInvalidRecordsAndOrphanTasks(String clusterARN, String hostedZoneId)
-    {
-        List<String> orphanTaskIPs = getTaskIPs(clusterARN);
-        Map<String, String> recordsMap = getRecordNameIpMap(hostedZoneId);
-
-        List<String> invalidRecords = new ArrayList<>();
-
-        List<Change> changes = new ArrayList<>();
-
-        for (Map.Entry<String, String> entry : recordsMap.entrySet())
-        {
-            String recordName = entry.getKey();
-            String recordIP = entry.getValue();
-
-            //Remove recordIP from orphanTaskIPs and
-            //if it failed to remove (the recordIP was not present
-            //in orphanTaskIPs) then we add the recordName
-            //to invalidRecords
-            if(!orphanTaskIPs.remove(recordIP))
-            {
-                invalidRecords.add(recordName);
-            }
-        }
-
-        for (String ip : orphanTaskIPs)
-        {
-            if(invalidRecords.size() > 0)
-            {
-                //Make a change to update an invalid record with
-                //an orphan IP
-
-                Change change = new Change();
-                ResourceRecordSet resourceRecordSet = new ResourceRecordSet();
-
-                resourceRecordSet.setName(invalidRecords.get(0));
-                resourceRecordSet.setType("A");
-                resourceRecordSet.setTTL((long) 300);
-
-                ResourceRecord resourceRecord = new ResourceRecord();
-                resourceRecord.setValue(ip);
-                resourceRecordSet.setResourceRecords(Collections.singletonList(resourceRecord));
-
-                change.setAction("UPSERT");
-                change.setResourceRecordSet(resourceRecordSet);
-
-                changes.add(change);
-            }
-            else
-            {
-                //Ran out of records to assign IPs
-                //Issue an error
-                break;
-            }
-        }
-
-        if(changes.size() > 0)
-        {
-            AmazonRoute53 r53 = AmazonRoute53ClientBuilder.defaultClient();
-
-            ChangeBatch changeBatch = new ChangeBatch();
-            changeBatch.setChanges(changes);
-
-            ChangeResourceRecordSetsRequest request = new ChangeResourceRecordSetsRequest();
-            request.setChangeBatch(changeBatch);
-            request.setHostedZoneId(hostedZoneId);
-
-            System.out.println(request);
-
-            r53.changeResourceRecordSets(request);
-        }
     }
 
 }
